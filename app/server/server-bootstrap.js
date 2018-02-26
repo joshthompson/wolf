68c6f3a80b559df2fcd5bd82d1556630f473fd90
Josh Thompson <josh3276@gmail.com>
2018-02-27 00:57:23 +0100

    Refactor

--- app/server/server-bootstrap.js
+++ app/server/server-bootstrap.js
@@ -1,138 +1,11 @@
-var WolfGame = require('../common/models/wolf-game.model')
-var Player = require('../common/models/player.model')
+var WolfGameSever = require('./wolf-game-server')
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
-// Socket.io Test
-let i = 0
 let games = {}
-
-io.on('connection', (socket) => {
-	
-	console.log('a user connected')
-
-	let game = null;
-	let player = null;
-
-	socket.on('disconnect', (reason) => {
-		if (game && !game.activeSockets()) {
-			setTimeout(() => {
-				if (!game.activeSockets()) {
-					// Remove inactive game
-					game.end()
-					delete games[game.code]
-					delete game
-				}
-			}, 3 * 1000)
-		}
-	})
-
-	socket.on('createGame', () => {
-		game = new WolfGame({
-			socket: socket,
-			state: 'SETUP'
-		})
-		games[game.code] = game
-		socket.emit('gameCreated', {game: game.toPrivateJSON()})
-		console.log(`game created: ${game.code}`)
-	})
-
-	socket.on('recoverGame', data => {
-		game = games[data.code]
-		if (game) {
-			if (game.token === data.token) {
-				game.socket = socket
-				return socket.emit('recoverGame', {game: game.toPrivateJSON()})
-			}
-			let players = game.players.filter(player => player.token === data.token)
-			if (players[0]) {
-				player = players[0]
-				player.socket = socket
-				return socket.emit('recoverGame', {game: game.toPublicJSON(), player: player.toPrivateJSON()})
-			}
-		}
-	})
-
-	socket.on('endGame', data => {
-		if (!game) {
-			return socket.emit('gameError', {message: 'Couldn\'t find game'})
-		}
-		game.end()
-		delete games[game.code]
-		delete game
-	})
-
-	socket.on('joinGame', data => {
-		game = games[data.game.toUpperCase()]
-		
-		if (!game) {
-			return socket.emit('gameError', {message: 'Couldn\'t find game'})
-		}
-
-		if (data.player.length < 2) {
-			return socket.emit('gameError', {message: 'Name can\'t be at least 2 characters'})
-		}
-
-		if (data.player.length > 20) {
-			return socket.emit('gameError', {message: 'Name can\'t be longer than 20 characters'})
-		}
-
-		if (game.findPlayerByName(data.player)) {
-			return socket.emit('gameError', {message: 'Choose a different name'})
-		}
-
-		player = new Player({
-			name: data.player,
-			game: game,
-			state: 'SELECT_AVATAR',
-			socket: socket
-		})
-		game.addPlayer(player)
-		socket.emit('gameJoined', {game: game.toPublicJSON(), player: player.toPrivateJSON()})
-	})
-
-	socket.on('startGame', () => {
-		game.state = 'READY'
-		game.setupPlayerIdentities()
-		game.update()
-	})
-
-	socket.on('changeName', name => {
-		if (!game) {
-			return socket.emit('gameError', {message: 'Couldn\'t find game'})
-		}
-		if (!player) {
-			return socket.emit('gameError', {message: 'Couldn\'t find player'})
-		}
-		if (name.length < 2) {
-			return socket.emit('gameError', {message: 'Name can\'t be at least 2 characters'})
-		}
-
-		if (name.length > 20) {
-			return socket.emit('gameError', {message: 'Name can\'t be longer than 20 characters'})
-		}
-		player.setName(name)
-		game.update()
-	})
-
-	socket.on('selectAvatar', avatar => {
-		if (!game) {
-			return socket.emit('gameError', {message: 'Couldn\'t find game'})
-		}
-		if (!player) {
-			return socket.emit('gameError', {message: 'Couldn\'t find player'})
-		}
-		if (game.players.filter(player => player.avatar === avatar).length) {
-			return socket.emit('gameError', {message: 'Please select a different avatar'})
-		}
-		player.setAvatar(avatar)
-		player.setState('READY')
-		game.update()
-	})
-
-})
+io.on('connection', (socket) => new WolfGameSever(socket, games))
 
 http.listen(9653, () => {
 	console.log('\033c')
@@ -145,14 +18,14 @@ Yb, \`88     88     d8'             IP'\`Yb IP'\`Y
      88     88     88    ,ggggg,   I8 dP  I8 dP
      88     88     88   dP"  "Y8gggI8dP   I8dP
      Y8    ,88,    8P  i8'    ,8I  I8P    I8P
-      Yb,,d8""8b,,dP  ,d8,   ,d8' ,d8b,_ ,d8b,_
-       "88"    "88"   P"Y8888P"   8P'"Y88PI8"8888
+      Yb,,d8""8b,,d8, ,d8,   ,d8' ,d8b,_ ,d8b,_
+       "88"    "88"\`P88P"Y8888P"   8P'"Y88PI8"8888
                                           I8 \`8,
-    Created by Josh Thompson              I8  \`8,
-    Copyright 2018                        I8   8I
-    github.com/joshthompson/wolf          I8   8I
+     Created by Josh Thompson             I8  \`8,
+     Copyright 2018                       I8   8I
+     github.com/joshthompson/wolf         I8   8I
                                           I8, ,8'
-    listening on *:9653                    "Y8P'
+     listening on *:9653                   "Y8P'
 `)
 })
 app.use(express.static('public'))
---
+++ app/server/wolf-game-server.js
@@ -0,0 +1,151 @@
+var WolfGame = require('../common/models/wolf-game.model')
+var Player = require('../common/models/player.model')
+
+class WolfGameSever {
+
+	constructor(socket, games) {
+		this.socket = socket
+		this.games = games
+		this.mode = null
+		this.game = null
+		this.player = null
+		this.setupSocket()
+	}
+
+	setupSocket() {
+		// Common
+		this.socket.on('disconnect', (...args) => this.disconnect(...args))
+		this.socket.on('recoverGame', (...args) => this.recoverGame(...args))
+		// New connection
+		this.socket.on('joinGame', (...args) => this.joinGame(...args))
+		this.socket.on('createGame', (...args) => this.createGame(...args))
+		// Host
+		this.socket.on('startGame', (...args) => this.startGame(...args))
+		this.socket.on('endGame', (...args) => this.endGame(...args))
+		// Player
+		this.socket.on('changeName', (...args) => this.changeName(...args))
+		this.socket.on('selectAvatar', (...args) => this.selectAvatar(...args))
+	}
+
+	disconnect(reason) {
+		if (this.game && !this.game.activeSockets()) {
+			setTimeout(() => {
+				if (!this.game.activeSockets()) {
+					// Remove inactive game
+					this.game.end()
+					delete this.games[this.game.code]
+					delete this.game
+				}
+			}, 3 * 1000)
+		}
+	}
+
+	createGame() {
+		this.game = new WolfGame({
+			socket: this.socket,
+			state: 'SETUP'
+		})
+		this.games[this.game.code] = this.game
+		this.socket.emit('gameCreated', {game: this.game.toPrivateJSON()})
+		console.log(`Game created: ${this.game.code}`)
+	}
+
+	recoverGame(data) {
+		this.game = this.games[data.code]
+		if (this.game) {
+			if (this.game.token === data.token) {
+				this.game.socket = this.socket
+				return this.socket.emit('recoverGame', {game: this.game.toPrivateJSON()})
+			}
+			let players = this.game.players.filter(player => player.token === data.token)
+			if (players[0]) {
+				this.player = players[0]
+				this.player.socket = this.socket
+				return this.socket.emit('recoverGame', {
+					game: this.game.toPublicJSON(),
+					player: this.player.toPrivateJSON()
+				})
+			}
+		}
+	}
+
+	endGame(data) {
+		if (!game) {
+			return this.socket.emit('gameError', {message: 'Couldn\'t find game'})
+		}
+		game.end()
+		delete this.games[this.game.code]
+		delete this //this.game
+	}
+
+	joinGame(data) {
+		this.game = this.games[data.game.toUpperCase()]
+		
+		if (!this.game) {
+			return this.socket.emit('gameError', {message: 'Couldn\'t find game'})
+		}
+
+		if (data.player.length < 2) {
+			return this.socket.emit('gameError', {message: 'Name can\'t be at least 2 characters'})
+		}
+
+		if (data.player.length > 20) {
+			return this.socket.emit('gameError', {message: 'Name can\'t be longer than 20 characters'})
+		}
+
+		if (this.game.findPlayerByName(data.player)) {
+			return this.socket.emit('gameError', {message: 'Choose a different name'})
+		}
+
+		this.player = new Player({
+			name: data.player,
+			game: this.game,
+			state: 'SELECT_AVATAR',
+			socket: this.socket
+		})
+		this.game.addPlayer(this.player)
+		this.socket.emit('gameJoined', {game: this.game.toPublicJSON(), player: this.player.toPrivateJSON()})
+	}
+
+	startGame() {
+		this.game.state = 'READY'
+		this.game.setupPlayerIdentities()
+		this.game.update()
+	}
+
+	changeName(name) {
+		if (!thiis.game) {
+			return this.socket.emit('gameError', {message: 'Couldn\'t find game'})
+		}
+		if (!this.player) {
+			return this.socket.emit('gameError', {message: 'Couldn\'t find player'})
+		}
+		if (name.length < 2) {
+			return this.socket.emit('gameError', {message: 'Name can\'t be at least 2 characters'})
+		}
+
+		if (name.length > 20) {
+			return this.socket.emit('gameError', {message: 'Name can\'t be longer than 20 characters'})
+		}
+		this.player.setName(name)
+		this.game.update()
+	}
+
+	selectAvatar(avatar) {
+		if (!this.game) {
+			return this.socket.emit('gameError', {message: 'Couldn\'t find game'})
+		}
+		if (!this.player) {
+			return this.socket.emit('gameError', {message: 'Couldn\'t find player'})
+		}
+		if (this.game.players.filter(player => this.player.avatar === avatar).length) {
+			return this.socket.emit('gameError', {message: 'Please select a different avatar'})
+		}
+		this.player.setAvatar(avatar)
+		this.player.setState('READY')
+		this.game.update()
+	}
+
+}
+
+module.exports = WolfGameSever
